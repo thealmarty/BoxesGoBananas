3cb17e2415399050185004af286a0644ab6709a2
Marty Stumpf <thealmartyblog@gmail.com>
2019-12-31 12:10:31 -0800

    Add Iterable class and show function.

--- HigherOrderPolymorphism.hs
+++ HigherOrderPolymorphism.hs
@@ -1,6 +1,11 @@
-{-# LANGUAGE ExplicitForAll #-}
-{-# LANGUAGE LambdaCase     #-}
-{-# LANGUAGE Rank2Types     #-}
+{-# LANGUAGE ExplicitForAll         #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE LambdaCase             #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE Rank2Types             #-}
+{-# LANGUAGE TypeSynonymInstances   #-}
+{-# LANGUAGE UndecidableInstances   #-}
 
 -- Extending Fegaras and Sheard's catamorphism
 -- using higher order polymorphism
@@ -48,8 +53,38 @@ countvar :: Exp Int -> Int
 countvar = cata countvarAux
 
 -- make the type parameter of Exp abstract
+-- rule out badcase and badcata as in fsCata
 iter0 :: (ExpF b -> b) -> (forall a. Exp a) -> b
 iter0 = cata
+
+openiter1 :: (ExpF b -> b) -> (Exp b -> Exp b) -> (b -> b)
+openiter1 f x y = cata f (x (Place y))
+
+-- rule out all unsound expressions
+iter1 :: (ExpF b -> b) -> (forall a. Exp a -> Exp a) -> (b -> b)
+iter1 = openiter1
+
+freevarused :: (forall a. Exp a -> Exp a) -> Bool
+freevarused e =
+  iter1
+    (\case
+       App x y -> x || y
+       Lam f -> f False)
+    e
+    True
+
+class Iterable a m n | m -> a, m -> n where
+  openiter :: (ExpF a -> a) -> m -> n
+  uniter :: (ExpF a -> a) -> n -> m
+
+instance Iterable a (Exp a) a where
+  openiter = cata
+  uniter f = Place
+
+instance (Iterable a m1 n1, Iterable a m2 n2) =>
+         Iterable a (m1 -> m2) (n1 -> n2) where
+  openiter f x = openiter f . x . uniter f
+  uniter f x = uniter f . x . openiter f
 
 -- Example expressions
 identity :: Exp a
@@ -58,3 +93,15 @@ identity = lam id
 -- \x.x (\x.x)
 idAppid :: Exp a
 idAppid = app identity identity
+
+vars :: [String]
+vars =
+  [[i] | i <- ['a' .. 'z']] <>
+  [i : Prelude.show j | j <- [1 ..], i <- ['a' .. 'z']]
+
+showAux :: ExpF ([String] -> String) -> ([String] -> String)
+showAux (App x y) vars   = "(" <> x vars <> " " <> y vars <> ")"
+showAux (Lam z) (v:vars) = "(fn " <> v <> ". " <> z (const v) vars <> ")"
+
+show :: (forall a. Exp a) -> String
+show e = iter0 showAux e vars
